ffdb1f1189cfba22aaa756747010d67d98491f50
Cedric <cedric.ameline@epsi.fr>
2019-06-18 12:15:28 +0200

    ORM refait

---
+++ ORM/ProjetBdd/ProjetBdd/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetBdd
+{
+    public class Class1
+    {
+    }
+}
